8968e27b501f6d8e5b247e953b5da2ddafd40f8b
Yobato <satriyobagus30077@gmail.com>
2022-02-22 00:20:11 +0700

    Carousel update 2

---
+++ Challenge/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    dots: false,
+    margin: 10,
+    nav: true,
+    navText: [
+      "<i class='fa-solid fa-angle-left'></i>",
+      "<i class='fa-solid fa-angle-right'></i>",
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+});
